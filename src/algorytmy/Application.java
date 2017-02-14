718a4b41a069c8af5a48f4f9e253fd0c4a9d505b
unknown <duneval@gmail.com>
2017-02-14 18:08:29 +0100

    Initial commit

---
+++ src/algorytmy/Application.java
@@ -0,0 +1,11 @@
+package algorytmy;
+
+/**
+ * Created by karamba on 2017-02-14.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}

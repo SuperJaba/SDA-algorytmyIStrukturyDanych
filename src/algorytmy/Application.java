ce889732d668c3e37d82c1464d699e9ff0c38a2a
unknown <duneval@gmail.com>
2017-02-15 19:42:54 +0100

    Added basic methods to MyList class

--- src/algorytmy/Application.java
+++ src/algorytmy/Application.java
@@ -1,11 +1,24 @@
 package algorytmy;
 
+import algorytmy.structures.MyList;
+
 /**
  * Created by karamba on 2017-02-14.
  */
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
+        MyList myList = new MyList();
+        myList.add(2);
+        myList.add(3);
+        myList.add(-3);
+        myList.add(-1);
+        myList.add(0);
+        myList.add(2);
+        myList.add(5);
+        myList.add(5);
+        myList.add(5);
+        myList.add(4);
+        myList.add(-4);
+        System.out.println(myList.get(3));
     }
 }
